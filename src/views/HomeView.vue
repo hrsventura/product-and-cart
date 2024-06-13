627c557a8c1dc83b7572e52da46404fdab7d3725
hrs.ventura.programming@gmail.com <hrs.ventura.programming@gmail.com>
2024-06-13 11:25:55 +0100

    .

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class="home">Home</div>
+  <div class="home">
+    <div class="splash-container">
+      <div class="splash">
+        <h1>Splendid Food</h1>
+      </div>
+    </div>
+    <main class="wrapper">
+      <h2>Recommended</h2>
+      <div class="recommended">
+        <ProductCard
+          v-for="(product, index) in inventory.slice(0, 3)"
+          :key="product.id"
+          class="card"
+          :index="index"
+          :product="product"
+          :addToCart="addToCart"
+        />
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
+import ProductCard from '@/components/Product-card.vue'
+
 export default {
   name: 'HomeView',
-  components: {
-  }
+  props: ['inventory', 'addToCart'],
+  components: { ProductCard }
 }
 </script>
